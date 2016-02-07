7c675216f831a3055b306a26cabf0b64f0c917fa
James Swineson <jamesswineson@gmail.com>
2016-02-07 17:09:34 +0800

    remove unused js

--- js/enable-han.js
+++ js/enable-han.js
@@ -1,15 +1,1 @@
-(function enablehan(fn) {
-  if (document.readyState != 'loading') {
-    fn();
-  } else if (document.addEventListener) {
-    document.addEventListener('DOMContentLoaded', fn);
-  } else {
-    document.attachEvent('onreadystatechange', function() {
-      if (document.readyState != 'loading')
-        fn();
-    });
-  }
-})(function() {
-  document.documentElement.classList.add('han-init');
-  Han.init();
-});
+document.documentElement.classList.add('han-init');
