a8dda7fc3956bace7420111de13718bd8933a8b8
James Swineson <jamesswineson@gmail.com>
2016-02-07 17:02:18 +0800

    Use JS to invoke han.js

--- js/enable-han.js
+++ js/enable-han.js
@@ -1,2 +1,15 @@
-var root = document.documentElement;
-root.classList.add('han-init');
+(function enablehan(fn) {
+  if (document.readyState != 'loading') {
+    fn();
+  } else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', fn);
+  } else {
+    document.attachEvent('onreadystatechange', function() {
+      if (document.readyState != 'loading')
+        fn();
+    });
+  }
+})(function() {
+  document.documentElement.classList.add('han-init');
+  Han.init();
+});
