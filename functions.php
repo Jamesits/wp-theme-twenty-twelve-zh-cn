db1ebaced176cb1285254a6e4d39344748a6a030
James Swineson <jamesswineson@gmail.com>
2016-02-07 17:04:30 +0800

    fix dep

--- functions.php
+++ functions.php
@@ -27,6 +27,6 @@
         array('parent-style')
     );
     wp_enqueue_script('enable-han-js', get_stylesheet_directory_uri().'/js/enable-han.js');
-    wp_enqueue_script('han-js', get_stylesheet_directory_uri().'/bower_components/Han/dist/han.min.js', array( 'twentytwelve-zhcn-js' ), false, true);
+    wp_enqueue_script('han-js', get_stylesheet_directory_uri().'/bower_components/Han/dist/han.min.js', array( 'enable-han-js' ));
  }
  add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
